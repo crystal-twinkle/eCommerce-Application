108de5035892269d5e6c7b45cc1e466f6cc7021e
Kristalik8 <autoro1@mail.ru>
2023-08-11 19:17:27 +0300

    fix: add type for paramets

--- src/shared/lib/validate-email.ts
+++ src/shared/lib/validate-email.ts
@@ -1,4 +1,4 @@
-export default function validateEmail(email) {
+export default function validateEmail(email: string) {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailPattern.test(email);
 }
--- src/shared/lib/validate-password.ts
+++ src/shared/lib/validate-password.ts
@@ -1,4 +1,4 @@
-export default function validatePassword(password) {
+export default function validatePassword(password: string) {
   const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^&*])[A-Za-z\d@$!%^&*]{8,}$/;
   return passwordPattern.test(password);
 }
