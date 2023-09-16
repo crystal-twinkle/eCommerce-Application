96f154d9b06b2862e11622625f0a8a4aefd1c14d
Kristalik8 <autoro1@mail.ru>
2023-09-16 09:03:36 +0300

    feat: add field for promo code

--- src/features/cart/cart-list.ts
+++ src/features/cart/cart-list.ts
@@ -6,10 +6,19 @@ import EmptyView from '../../shared/ui/empty-view/empty-view';
 import CartListCard from './cart-list-card';
 import './cart.scss';
 import store from '../../app/store';
+import Input from '../../shared/ui/input/input';
+import Button from '../../shared/ui/button/button';
+import CartApi from '../../entities/cart/cart-api';
+import { ButtonType } from '../../shared/ui/button/models';
 
 export default class CartList extends CommonBuilderWrapper {
   private loader: Loader;
   private emptyView: EmptyView;
+  private cartCards: HTMLElement[];
+  private costContainer: ElementBuilder;
+  private cost: ElementBuilder;
+  private clearButton: Button;
+  private whitelist: ElementBuilder;
 
   constructor() {
     super();
@@ -20,6 +29,10 @@ export default class CartList extends CommonBuilderWrapper {
       tag: 'div',
       styleClass: 'cart-list',
     });
+    this.whitelist = new ElementBuilder({
+      tag: 'div',
+      styleClass: 'cart-list__whitelist',
+    });
   }
 
   public setCart(cart: Cart): void {
@@ -28,11 +41,37 @@ export default class CartList extends CommonBuilderWrapper {
       this.empty();
       return;
     }
-    this.builder.append(this.setCards());
+    this.cartCards = cart.lineItems.map((item: LineItem) => new CartListCard(item).getElement());
+
+    this.costContainer = new ElementBuilder({
+      tag: 'div',
+      styleClass: 'cart-list-card__cost-container cart-list-card__cost-container_total',
+      content: `Total:`,
+    });
+
+    this.cost = new ElementBuilder({
+      tag: 'div',
+      styleClass: 'cart-list-card__cost',
+      content: `$${(store.cart.totalPrice.centAmount / 100).toFixed(2)}`,
+    });
+    this.costContainer.append([this.cost.getElement()]);
+
+    this.clearButton = new Button({
+      callback: async () => {
+        CartApi.clearCart();
+      },
+      type: ButtonType.DEFAULT,
+      text: 'Clear Cart',
+    });
+    this.clearButton.getElement().classList.add('cart-list-card__button_clear');
+    this.builder.append(this.cartCards);
+    this.whitelist.append([this.setPromo(), this.costContainer.getElement(), this.clearButton.getElement()]);
+    this.builder.getElement().after(this.whitelist.getElement());
   }
 
-  public setCards(): HTMLElement[] {
-    return store.cart.lineItems.map((item: LineItem) => new CartListCard(item).getElement());
+  public setPromo(): HTMLElement {
+    const promocode = new Input({ styleClass: 'cart-list__promocode', placeholder: 'Promo code' });
+    return promocode.getElement();
   }
 
   public empty(): void {
